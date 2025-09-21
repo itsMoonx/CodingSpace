fdba2a6847b29b41448e0575c496b8d6893b4e9d
Judy <judy2solo@gmail.com>
2025-09-21 17:31:37 +0100

    Made the "add" button create a new checkbox item.

--- todolist/src/main/Main.java
+++ todolist/src/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
-public class Main {
+import controllers.todolist;
+
+public class Main extends todolist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		launch(args);
 	}
 
 }
